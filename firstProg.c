867dc439756b297c66a403d692c0ca21f2fbabbe
Ms01000 <mohamed.serraye01@gmail.com>
2020-03-12 22:20:28 +0100

    mise à jour des commentaires

--- firstProg.c
+++ firstProg.c
@@ -1,11 +1,16 @@
 // Premier programme 
 
+
+//fihier de librairie pour la fonction printf  
 #include <stdio.h>
 
+
+// Point d'entré  du programme 
 int main (void)
-{ 
+{
 
 // Un simple affichage 
+
 printf(" Bonjour c'est mon premier progamme ") ; 
 
 }
